ba4fec8885615a932246cc22f9c5e4a03605a790
Yannick Bijl <yannick.bijl@gmail.com>
2025-12-09 13:13:33 +0100

    Up parallel upload of file from 3 to 6

--- novaseqx/upload_finished_analysis_files.sh
+++ novaseqx/upload_finished_analysis_files.sh
@@ -4,7 +4,7 @@
 export SERVER_URL="https://upload.test.hartwigmedicalfoundation.nl"
 export AUTH_TOKEN="enter-your-token"
 
-MAX_PARALLEL_UPLOADS=3
+MAX_PARALLEL_UPLOADS=6
 OTHER_FILES=("Quality_Metrics.csv" "SampleSheet.csv" "RunInfo.xml")
 RUN_DIRECTORY="$1"
 RUN_NAME="$2"
