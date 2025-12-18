757cf708e552ad7fdf4c6e7d3265bac362ccdbbe
arneroeters <a.roeters@hartwigmedicalfoundation.nl>
2025-12-18 12:13:40 +0100

    INFRA-1961: test

--- novaseqx/monitor_conversion_ready.sh
+++ novaseqx/monitor_conversion_ready.sh
@@ -11,16 +11,17 @@ ANALYSIS_COMPLETE_FILE="Analysis/1/Data/Secondary_Analysis_Complete.txt"
 
 process_folder() {
     local file="$1"
+    echo "Processing: $file"
     local dir="${file%%"$ANALYSIS_COMPLETE_FILE"}"
     local run_name="${dir##"$BASE_DIR"/}"
     echo "Processing run: $run_name"
-    "./upload_finished_analysis_files.sh" "$dir" "$run_name"
+#    "./upload_finished_analysis_files.sh" "$dir" "$run_name"
 }
 
 echo "Monitoring $BASE_DIR for new $ANALYSIS_COMPLETE_FILE files"
-inotifywait -m -r -e close_write --format '%w%f' "$BASE_DIR" | while read -r file; do
-  echo "$file"
-    if [[ "$file" == *"$ANALYSIS_COMPLETE_FILE" ]]; then
-        process_folder "$file"
+inotifywait -m -r -e close_write --format '%w%f' "$BASE_DIR" | while read -r full_path; do
+  echo "$full_path"
+    if [[ "$full_path" == *"$ANALYSIS_COMPLETE_FILE" ]]; then
+        process_folder "$full_path"
     fi
 done
