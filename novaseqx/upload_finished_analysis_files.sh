a9013c79242bd5664c3d0c4b83078f25b404acfe
arneroeters <a.roeters@hartwigmedicalfoundation.nl>
2025-12-18 13:59:56 +0100

    INFRA-1961: improve logging

--- novaseqx/upload_finished_analysis_files.sh
+++ novaseqx/upload_finished_analysis_files.sh
@@ -46,12 +46,11 @@ upload_files() {
     local uri_base="novaseq/$FLOWCELL_ID/$folder"
     echo "Starting to upload $pattern files to $uri_base"
 
-    echo "Script is running from: $(pwd)"
     files=()
     while IFS= read -r file; do
         files+=("$file:$(get_sub_path "$file" "$folder_depth")")
     done < <(find "$FLOWCELL_DATA_DIRECTORY" -type f -name "$pattern")
-    echo "Uploading ${#files[@]} file(s)"
+    echo "Found ${#files[@]} $pattern file(s)"
     printf "%s\n" "${files[@]}" | parallel -j $MAX_PARALLEL_UPLOADS -C ':' './upload-file.sh' {1} "$uri_base"/{2}
     wait
 
