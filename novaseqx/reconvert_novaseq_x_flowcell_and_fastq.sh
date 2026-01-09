4ee76d75a1208d1825ecb64ca2ac5ff290f220b7
Yannick Bijl <y.bijl@hartwigmedicalfoundation.nl>
2026-01-09 10:34:23 +0100

    DEV-4255: use .q30 instead .yld

--- novaseqx/reconvert_novaseq_x_flowcell_and_fastq.sh
+++ novaseqx/reconvert_novaseq_x_flowcell_and_fastq.sh
@@ -85,7 +85,7 @@ do
 
     fastq_api=$(hmf_api_get "fastq?sample_id=${sample_id}")
     sample_yld=$(echo "${fastq_api}" | jq -r '.[] | select(.qc_pass==true) | select(.bucket!= null) | .yld' | awk '{sum+=$0} END {printf "%.0f", sum}')
-    sample_q30=$(echo "${fastq_api}" | jq -r '.[] | select(.qc_pass==true) | select(.bucket!= null) | .yld' | awk '{sum+=$0; ++n} END {print sum/n}')
+    sample_q30=$(echo "${fastq_api}" | jq -r '.[] | select(.qc_pass==true) | select(.bucket!= null) | .q30' | awk '{sum+=$0; ++n} END {print sum/n}')
     if [[ $(echo "${sample_yld} >= ${sample_yld_req}" | bc -l) -eq 1 && $(echo "${sample_q30} >= ${sample_q30_req}" | bc -l) -eq 1 ]]
     then
         sample_status="Ready"
